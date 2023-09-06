d027e107c063c66a871d680250239309ebf0c1fb
seongmin <seongm22n@gmail.com>
2023-09-06 13:03:50 +0900

    remove: 불필요한 페이지 삭제

--- src/pages/SearchInfo.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { useParams } from 'react-router-dom';
-
-const SearchInfo: React.FC = () => {
-	const { keyword } = useParams();
-	return <div>검색한 결과: {keyword}</div>;
-};
-export default SearchInfo;
