62cfb5331937433d54105b8d6b0c90378ad371f9
seongmin <seongm22n@gmail.com>
2023-09-06 13:03:36 +0900

    feat : main 페이지 추가

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,7 +1,21 @@
-import React from 'react';
+import SearchSick from '../components/search/SearchSick';
 
-const Main: React.FC = () => {
-	return <div>검색 Input이 보여야 함</div>;
-};
+function Main() {
+	return (
+		<div>
+			<div>
+				<div>
+					<p>
+						국내 모든 임상시험 검색하고 <br /> 온라인으로 참여하기
+					</p>
+					{/* 이미지 추가하려면 하기 */}
+				</div>
+				<div>
+					<SearchSick />
+				</div>
+			</div>
+		</div>
+	);
+}
 
 export default Main;
