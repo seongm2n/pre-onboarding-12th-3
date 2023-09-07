9e7c583427de53e868a0dad81865f149d5177d38
seongmin <seongm22n@gmail.com>
2023-09-07 14:51:48 +0900

    feat: 로컬 캐싱 추가

---
+++ src/lib/cache/localCache.ts
@@ -0,0 +1,36 @@
+import { SickData } from 'sickType';
+
+const localCache = (() => {
+	const writeToCache = (key: string, data: SickData[]) => {
+		const timestampInMinutes = Math.floor(new Date().getMinutes());
+		const storageValue = {
+			data,
+			timestamp: timestampInMinutes,
+		};
+
+		localStorage.setItem(key, JSON.stringify(storageValue));
+	};
+
+	const readFromCache = (key: string) => {
+		const storageValueString = localStorage.getItem(key);
+		if (!storageValueString) return [];
+
+		const storageValue = JSON.parse(storageValueString);
+
+		if (new Date().getMinutes() - storageValue.timestampInMinutes > EXPIRE_TIME) {
+			localStorage.removeItem(key);
+			return [];
+		}
+
+		return storageValue.recommendations || [];
+	};
+
+	return {
+		writeToCache,
+		readFromCache,
+	};
+})();
+
+export default localCache;
+
+const EXPIRE_TIME = 60 * 1000; // 5분
